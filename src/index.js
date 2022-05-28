cf87adacbacf6aeed4c853d79e88e1d589589869
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-28 18:27:25 +0300

    Start npm
    Add npm ppack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+const axios = require('axios');
+
+const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.load-more');
+
+// BackEnd
